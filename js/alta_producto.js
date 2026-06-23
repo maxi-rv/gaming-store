157bb7df8f693e1d7b520891dcde59f967eb3441
dario pokorny <dario.pokorny@gmail.com>
2026-06-23 18:39:09 -0300

    alta de producto

--- js/alta_producto.js
+++
@@ -1,7 +0,0 @@
-let name_producto = document.getElementById("input_nombre_producto")
-let precio_producto = document.getElementById("input_precio")
-let stock_producto = document.getElementById("input_stock")
-
-window.addEventListener("load", function(){
-    inicializar()
-})
