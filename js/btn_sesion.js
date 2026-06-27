02343593a5a1f2e81651d266364a540ec9a079af
dario pokorny <dario.pokorny@gmail.com>
2026-06-27 07:28:09 -0300

    oculta btn de admin

--- js/btn_sesion.js
+++ js/btn_sesion.js
@@ -1,19 +1,23 @@
 import {
     consultar_sesion,
     cerrar_sesion,
+    conseguir_rol_usuario,
 } from "../js/gestores/gestor_sesion.js";
 
 let txt_sesion = document.getElementById("txt_sesion")
+let btn_accion_admin = document.getElementById("buttonAdminActions")
 
 window.addEventListener("load", function(){
     modificar_btn_sesion()
-    
+    modificar_btn_rol()
+
     document.getElementById("linkIniciarSesion").addEventListener("click", function(i){
         if (consultar_sesion() === true){
             window.location.href = "../html/inicioSesion.html";
              i.preventDefault();
             cerrar_sesion()
             modificar_btn_sesion()
+            modificar_btn_rol()
         }
     } )
 
@@ -28,5 +32,11 @@ function modificar_btn_sesion(){
         } else {
             txt_sesion.textContent = "Iniciar sesion";
         }
+    }
+}
+
+function modificar_btn_rol(){
+    if (conseguir_rol_usuario() === "admin"){
+        btn_accion_admin.style.visibility = "visible"
     }
 }
